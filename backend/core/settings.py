37adfbb31f72daa40787cf67725c60a63e7f3913
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2025-07-22 23:29:34 +0100

    db setup done

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -70,10 +70,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
+        'HOST': 'localhost',
+        'PORT': env('DB_PORT')
     }
 }
 
