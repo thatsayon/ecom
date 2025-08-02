3f5b379fee5bdac84caa319594dcc0056e973268
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2025-08-02 01:45:51 +0100

    tried to dockerize the project

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASS'),
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': env('DB_PORT')
     }
 }
